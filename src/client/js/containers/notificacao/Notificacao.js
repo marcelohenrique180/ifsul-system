9aa417ac744e19727f16f4e4c55eaf39dcab33cc
Marcelo <marcelohs180@gmail.com>
2017-04-13 23:35:52 -0300

    Ref(notificacao)

--- src/client/js/containers/notificacao/Notificacao.js
+++ src/client/js/containers/notificacao/Notificacao.js
@@ -1,27 +1,68 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router'
 import {requestNotificacao} from '../../actions/notificacao'
 
+import {getRequerimento} from '../../actions/requerimento'
+import {requestTipos} from '../../actions/tipo'
+import {getAluno} from '../../actions/aluno'
+import {requestCursos} from '../../actions/curso'
+
+require('../../../scss/notificacoes.scss');
+
 class Notificacao extends React.Component{
+
     constructor(props){
         super(props);
+        this.props.dispatch(requestNotificacao());
+        this.update = this.update.bind(this);
+    }
 
-        this.props.dispatch(requestNotificacao())
+    update(locale){
+        return () => {
+            const {dispatch} = this.props;
+            const regex = /menu\/[a-z]+\/requerimento\/([0-9]+)/;
+            const match = regex.exec(locale);
+
+            if (match){
+                if(match[1]) {
+                    dispatch(getRequerimento(match[1])).then(
+                        requerimento => {
+                            dispatch(requestTipos(requerimento.response._links.tipo.href));
+                            dispatch(getAluno(requerimento.response._links.aluno.href)).then(
+                                aluno => {
+                                    dispatch(requestCursos(aluno.response._links.curso.href))
+                                }
+                            );
+                        }
+                    );
+                }
+            }
+        }
     }
 
     render(){
-        const notificacao = this.props.notificacao;
+        const {notificacao, base} = this.props;
         let mensagem = "";
 
         if (notificacao.isFetching){
             mensagem = <div style={{textAlign: "center"}}>Carregando...</div>
         }else if (notificacao.fetched){
             const todasNotificacoes = notificacao.notificacao.notificacoes;
-            if (todasNotificacoes.lenght == 0) {
+            if (todasNotificacoes.length == 0) {
                 mensagem = "Sem novas Notificações!"
             }else {
-                mensagem = todasNotificacoes.map(singleNotificacao => {
-                    return  <li key={todasNotificacoes.indexOf(singleNotificacao)} className="list-group-item">{singleNotificacao.mensagem}</li>
+                mensagem = todasNotificacoes.map(notificacao => {
+                    return (
+                        <Link to={base+notificacao.link} key={todasNotificacoes.indexOf(notificacao)}>
+                            <li
+                                onClick={this.update(base+notificacao.link)}
+                                className="list-group-item"
+                                style={{backgroundColor: notificacao.color}}>
+                                {notificacao.mensagem}
+                            </li>
+                        </Link>
+                    )
                 })
             }
         }else if (notificacao.error){
