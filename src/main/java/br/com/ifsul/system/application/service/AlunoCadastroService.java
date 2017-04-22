d41e1a30b39800565f64c823a213483e366e0bbc
Marcelo <marcelohs180@gmail.com>
2017-04-21 22:50:08 -0300

    Doc(aluno-cadastro-service)
    Comenta lógica de confirmação de Aluno

--- src/main/java/br/com/ifsul/system/application/service/AlunoCadastroService.java
+++ src/main/java/br/com/ifsul/system/application/service/AlunoCadastroService.java
@@ -1,16 +1,13 @@
 package br.com.ifsul.system.application.service;
 
 import br.com.ifsul.system.infrastructure.database.dao.AlunoDAO;
-import br.com.ifsul.system.infrastructure.database.dao.UsuarioDAO;
 import br.com.ifsul.system.infrastructure.database.dao.VerificationTokenDAO;
 import br.com.ifsul.system.infrastructure.errorhandling.ApiError;
 import br.com.ifsul.system.pojo.Aluno;
-import br.com.ifsul.system.pojo.Usuario;
 import br.com.ifsul.system.pojo.VerificationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.BindingResult;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -23,24 +20,29 @@ public class AlunoCadastroService {
     private SendConfirmEmail sender;
 
     public void confirmarAluno(Aluno aluno){
+        // verifica se aluno existe
         aluno = alunoDAO.findByMatricula(aluno.getMatricula());
         if (aluno == null){
             throw new ApiError(HttpStatus.NOT_FOUND, "Aluno não encontrado", "Aluno");
         }
 
-        VerificationToken verificationToken = new VerificationToken(UUID.randomUUID().toString(), aluno.getUsuario());
         try {
+            VerificationToken verificationToken;
             VerificationToken dbToken = tokenDAO.findTokenByUsuario(aluno.getUsuario().getEmail());
 
+            // se token já existe
             if (dbToken != null){
                 if (dbToken.isVerified()){
                     throw new ApiError(HttpStatus.BAD_REQUEST, "Usuário já cadastrado", "Aluno");
                 }else{
-                    verificationToken.setId(dbToken.getId());
+                    // reenvia email
+                    verificationToken = dbToken;
                 }
+            } else {
+                // cria novo token
+                verificationToken = new VerificationToken(UUID.randomUUID().toString(), aluno.getUsuario());
+                verificationToken = tokenDAO.save(verificationToken);
             }
-
-            verificationToken = tokenDAO.save(verificationToken);
 
             try {
                 sender.enviar(aluno.getUsuario(), verificationToken);
