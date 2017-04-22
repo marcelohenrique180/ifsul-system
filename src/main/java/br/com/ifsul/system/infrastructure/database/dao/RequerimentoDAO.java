4c4ecbc89b4c9d1ed082ede790d4abaefda2ce32
Marcelo <marcelohs180@gmail.com>
2017-04-22 20:09:25 -0300

    Ref(requerimento-dao)
    Change to Paging and Sorting Repo type.

--- src/main/java/br/com/ifsul/system/infrastructure/database/dao/RequerimentoDAO.java
+++ src/main/java/br/com/ifsul/system/infrastructure/database/dao/RequerimentoDAO.java
@@ -1,10 +1,10 @@
 package br.com.ifsul.system.infrastructure.database.dao;
 
 import br.com.ifsul.system.pojo.Requerimento;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 @RepositoryRestResource(collectionResourceRel = "requerimentos", path = "requerimentos")
-public interface RequerimentoDAO extends CrudRepository<Requerimento, Long> {
+public interface RequerimentoDAO extends PagingAndSortingRepository<Requerimento, Long> {
 }
