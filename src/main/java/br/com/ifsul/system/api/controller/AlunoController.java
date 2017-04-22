1900a09eb03347d53fa98f99cd45a1ffc1cb1ea5
Marcelo <marcelohs180@gmail.com>
2017-04-21 21:24:00 -0300

    clean(aluno-controller)
    Remove linhas desnecessárias ou sem uso.

--- src/main/java/br/com/ifsul/system/api/controller/AlunoController.java
+++ src/main/java/br/com/ifsul/system/api/controller/AlunoController.java
@@ -3,45 +3,28 @@ package br.com.ifsul.system.api.controller;
 import br.com.ifsul.system.application.service.AlunoCadastroService;
 import br.com.ifsul.system.application.service.AlunoConfirmarService;
 import br.com.ifsul.system.application.service.SendConfirmEmail;
-import br.com.ifsul.system.application.service.VerificationTokenService;
-import br.com.ifsul.system.infrastructure.database.dao.AlunoDAO;
 import br.com.ifsul.system.infrastructure.errorhandling.ApiError;
 import br.com.ifsul.system.pojo.Aluno;
 import br.com.ifsul.system.pojo.Usuario;
-import br.com.ifsul.system.pojo.VerificationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.rest.webmvc.PersistentEntityResource;
-import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
-import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.ResourceProcessor;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class AlunoController {
+class AlunoController {
 
     private AlunoCadastroService cadastroService;
-    private VerificationTokenService verificationTokenService;
     private AlunoConfirmarService confirmarService;
-
-    @Autowired
-    private RepositoryEntityLinks entityLinks;
 
     @Value("${spring.local.host}")
     private String url;
     private SendConfirmEmail sender;
-
-    private AlunoDAO alunoDAO;
 
 
     @RequestMapping(value = "/api/cadastro/aluno", method = RequestMethod.POST)
@@ -60,19 +43,6 @@ public class AlunoController {
         return usuario;
     }
 
-    //@RequestMapping(value = "/api/aluno", method = RequestMethod.POST)
-    // When It's needed to insert alunos
-    public Aluno alunoInsert(@RequestBody @Valid Aluno aluno, HttpServletRequest request, BindingResult result) throws ApiError {
-        String token = request.getHeader("VerificationToken");
-        verificationTokenService.setResult(result);
-        VerificationToken tokenFromDB = verificationTokenService.verificar(token);
-
-        aluno.setUsuario(tokenFromDB.getUsuario());
-        aluno = alunoDAO.save(aluno);
-        verificationTokenService.remover(tokenFromDB);
-        return aluno;
-    }
-
     @ExceptionHandler({ ApiError.class })
     public Map<String, String> handleAccessDeniedException(ApiError ex, HttpServletResponse response) {
         Map<String, String> errorMap = new HashMap<>();
@@ -81,16 +51,6 @@ public class AlunoController {
         response.setStatus(ex.getStatus().value());
 
         return errorMap;
-    }
-
-    @Autowired
-    public void setVerificationTokenService(VerificationTokenService verificationTokenService) {
-        this.verificationTokenService = verificationTokenService;
-    }
-
-    @Autowired
-    public void setAlunoDAO(AlunoDAO alunoDAO) {
-        this.alunoDAO = alunoDAO;
     }
 
     @Autowired
@@ -107,4 +67,17 @@ public class AlunoController {
     public void setSender(SendConfirmEmail sender) {
         this.sender = sender;
     }
+
+    /*@RequestMapping(value = "/api/aluno", method = RequestMethod.POST)
+    // When It's needed to insert alunos
+    public Aluno alunoInsert(@RequestBody @Valid Aluno aluno, HttpServletRequest request, BindingResult result) throws ApiError {
+        String token = request.getHeader("VerificationToken");
+        verificationTokenService.setResult(result);
+        VerificationToken tokenFromDB = verificationTokenService.verificar(token);
+
+        aluno.setUsuario(tokenFromDB.getUsuario());
+        aluno = alunoDAO.save(aluno);
+        verificationTokenService.remover(tokenFromDB);
+        return aluno;
+    }*/
 }
