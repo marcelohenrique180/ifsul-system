4e3a4fb6d5e05a2eba308d965f9969ab954d3b46
marcelohenrique180 <marcelohs180@gmail.com>
2017-06-09 12:07:08 -0300

    refactor(alerta): permite omissão do param show.

--- src/client/js/components/Alerta.js
+++ src/client/js/components/Alerta.js
@@ -4,10 +4,12 @@ export default class Alert extends React.Component{
     render(){
         const {show, message, alertClass} = this.props;
 
+        const shouldApear = typeof show === "undefined" ? true : show;
+
         return (
             <div>
                 {
-                    show &&
+                    shouldApear &&
                     <div>
                         <div className={alertClass+" alert text-center col-xs-8 col-xs-offset-2"} role="alert">
                             {message}
