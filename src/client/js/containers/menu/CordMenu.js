038a40e9f87a94e54a6d5862c507f368a3332900
marcelohenrique180 <marcelohs180@gmail.com>
2017-06-09 21:21:46 -0300

    style(cord-menu): bootstrap classes.

--- src/client/js/containers/menu/CordMenu.js
+++ src/client/js/containers/menu/CordMenu.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AuthorizedContainer from '../AuthorizedContainer'
-import Notificacao from '../notificacao/Notificacao'
+import CordRequerimentoAberto from '../requerimento/CordRequerimentoAberto'
 import RouterHandler from '../RouterHandler'
 
 class CordMenu extends AuthorizedContainer {
@@ -8,10 +8,10 @@ class CordMenu extends AuthorizedContainer {
     render(){
         return (
             <div>
-                <div className="col-xs-10 col-xs-offset-1 col-sm-4 col-lg-2 col-sm-offset-0">
-                    <Notificacao base="/menu/cordcurso" />
+                <div className="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-lg-3">
+                    <CordRequerimentoAberto />
                 </div>
-                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-sm-offset-1 col-md-offset-1 col-lg-offset-2">
+                <div className="col-xs-12 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1">
                     <RouterHandler {...this.props} />
                 </div>
             </div>
