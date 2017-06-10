a56fb33fe2289e75555265355a6bd36f15f48170
marcelohenrique180 <marcelohs180@gmail.com>
2017-06-09 21:22:19 -0300

    feat(requerimentos-abertos): criar método no servidor.

--- src/main/java/br/com/ifsul/infrastructure/database/dao/RequerimentoDAO.java
+++ src/main/java/br/com/ifsul/infrastructure/database/dao/RequerimentoDAO.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Set;
+
 
 @RepositoryRestResource(collectionResourceRel = "requerimentos", path = "requerimentos")
 public interface RequerimentoDAO extends PagingAndSortingRepository<Requerimento, Long> {
 
     @Query(value = "SELECT r FROM Requerimento r INNER JOIN r.aluno a INNER JOIN a.usuario u WHERE u.email = ?#{authentication.name}")
     Page<Requerimento> findAll(Pageable pageable);
+
+    Set<Requerimento> findRequerimentosByParecerIsNull();
 }
