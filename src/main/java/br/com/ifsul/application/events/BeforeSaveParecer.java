fd0d0203e669015d3e236578aae393d0e98810c4
marcelohenrique180 <marcelohs180@gmail.com>
2017-06-11 13:35:47 -0300

    refactor(b4-save-parecer): set departamento deferinte no req.

--- src/main/java/br/com/ifsul/application/events/BeforeSaveParecer.java
+++ src/main/java/br/com/ifsul/application/events/BeforeSaveParecer.java
@@ -2,7 +2,10 @@ package br.com.ifsul.application.events;
 
 import br.com.ifsul.api.jwt.AuthenticatedUser;
 import br.com.ifsul.infrastructure.database.dao.DepartamentoDAO;
+import br.com.ifsul.infrastructure.database.dao.RequerimentoDAO;
+import br.com.ifsul.pojo.Departamento;
 import br.com.ifsul.pojo.Parecer;
+import br.com.ifsul.pojo.Requerimento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
@@ -19,10 +22,20 @@ public class BeforeSaveParecer {
     @Autowired
     private DepartamentoDAO departamentoDAO;
 
+    @Autowired
+    private RequerimentoDAO requerimentoDAO;
+
     @HandleBeforeCreate
     public void handleBeforeCreate(Parecer parecer){
         AuthenticatedUser user = (AuthenticatedUser) SecurityContextHolder.getContext().getAuthentication();
         String username = user.getName();
+
+        Departamento departamentoDeferinte = departamentoDAO.findDepartamentoByUsuarioEmail(username);
         parecer.setDepartamento(departamentoDAO.findDepartamentoByUsuarioEmail(username));
+
+        Requerimento requerimento = parecer.getRequerimento();
+        requerimento.setDepartamentoDeferinte(departamentoDeferinte);
+
+        requerimentoDAO.save(requerimento);
     }
 }
