da7541fe84b51ef8283cf23d35fee37cb934e3b1
marcelohenrique180 <marcelohs180@gmail.com>
2017-06-11 13:34:05 -0300

    feat(paginator): criar component de paginação.
    Aplicável à qualquer entidade do spring data rest com paginação.

---
+++ src/client/js/components/Paginator.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router'
+import Carregando from '../components/Carregando'
+
+export default class Paginator extends Component {
+
+    render(){
+        const {pageableEntity, currentPage, onClickHandler, api} = this.props;
+        const {pathname} = this.props.location;
+
+        if(typeof pageableEntity.page !== "undefined"){
+            const {_links, page} = pageableEntity;
+            let pagination = null;
+
+            if (page.totalElements - 1 > 0) {
+                const itens = () => {
+                    const array = [];
+
+                    const pageControl = (() => {
+                        const {totalPages} = page;
+                        const pages = 6;
+                        const pageControl = {};
+
+                        if (pages >= totalPages) {
+                            pageControl.start = 0;
+                            pageControl.end = totalPages - 1;
+                        } else {
+                            if (totalPages - currentPage <= pages/2){//ending node
+                                pageControl.start = totalPages - pages - 1;
+                                pageControl.end = totalPages - 1;
+                            }
+                            else if (pages/2 > currentPage){//starting node
+                                pageControl.start = 0;
+                                pageControl.end = pages;
+                            } else {
+                                pageControl.start = currentPage - (pages/2);
+                                pageControl.end = currentPage + (pages/2);
+                            }
+                        }
+
+                        return pageControl
+                    })();
+
+                    for (let i=pageControl.start; i <= pageControl.end; i++){
+                        if (i !== currentPage){
+                            array.push(
+                                <li key={i}>
+                                    <Link to={pathname} onClick = {onClickHandler(api+"page="+(i))} >
+                                        {i+1}
+                                    </Link>
+                                </li>
+                            )
+                        } else {
+                            array.push(
+                                <li className="active" key={i}>
+                                    <Link to={pathname}
+                                          onClick = {() => { onClickHandler(api+"page="+(i))}} >
+                                        {i+1}
+                                    </Link>
+                                </li>
+                            )
+                        }
+                    }
+                    return array
+                };
+
+                if (_links.first){
+                    pagination =
+                        <div>
+                            <ul className="pagination">
+                                <li>
+                                    <Link to={pathname} onClick = {onClickHandler(_links.first.href)} >
+                                        &laquo;
+                                    </Link>
+                                </li>
+                                {itens()}
+                                <li>
+                                    <Link to={pathname} onClick = {onClickHandler(_links.last.href)} >
+                                        &raquo;
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                }
+            }
+
+            return pagination
+        }
+        return <Carregando />;
+    }
+}
