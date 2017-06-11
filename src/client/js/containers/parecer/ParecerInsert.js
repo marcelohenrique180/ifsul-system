298508161eb3e70bd2f1e51b6506cc4efb65bdc6
marcelohenrique180 <marcelohs180@gmail.com>
2017-06-11 00:35:23 -0300

    feat(parecer-insert): criar lógica de submissao de parecer.

--- src/client/js/containers/parecer/ParecerInsert.js
+++ src/client/js/containers/parecer/ParecerInsert.js
@@ -1,20 +1,49 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import autobind from 'autobind-decorator'
+import {sendParecer} from '../../actions/parecer'
+import {getRequerimentosEmAberto} from '../../actions/requerimento'
 import {handleChange} from '../../util'
+import {areFieldsEmpty} from '../../util'
 import FloatInput from '../../components/FloatInput'
+import Alerta from '../../components/Alerta'
 
-export default class ParecerInsert extends Component {
+class ParecerInsert extends Component {
+
     constructor(props){
         super(props);
 
-        this.state = {deferido: "", parecer: "", memorando: ""};
+        this.state = {deferido: "", parecer: "", memorando: "", erro: {message: "", erro: false}};
         this.handleChange = handleChange.bind(this);
     }
 
+    @autobind
+    handleSubmit(e){
+        e.preventDefault();
+
+        const {deferido, parecer, memorando} = this.state;
+        const notNullFields = [deferido, parecer, memorando];
+        const requerimento_link = this.props.requerimento.requerimento._links.self.href;
+
+        const error = areFieldsEmpty(notNullFields);
+        if (error){
+            this.setState({erro: {erro: true, message: "Algum campo obrigatório não foi preenchido."}});
+        }else {
+            this.setState({erro: {erro: false}});
+            this.props.sendParecer({deferido, parecer, memorando, requerimento: requerimento_link}).then(() => {
+                this.props.getRequerimentosEmAberto()
+            });
+            this.props.router.push("/");
+        }
+    }
+
     render(){
+        const {erro} = this.state;
+
         return (
             <div>
                 <h3 className="text-center">Parecer</h3>
-                <div className="form-group">
+                <form className="form-group">
                     <div className="form-group">
                         <label htmlFor="parecer">Parecer</label>
                         <textarea name="parecer" id="parecer" rows="5"
@@ -24,15 +53,15 @@ export default class ParecerInsert extends Component {
                     <div className="form-group radio-inline-flex">
                         <div className="radio-inline">
                             <label>
-                                <input type="radio" name="deferido" value="deferido"
-                                       checked={this.state.deferido === "deferido"}
+                                <input type="radio" name="deferido" value="true"
+                                       checked={this.state.deferido === "true"}
                                        onChange={this.handleChange}/>Deferir
                             </label>
                         </div>
                         <div className="radio-inline">
                             <label>
-                                <input type="radio" name="deferido" value="naodeferido"
-                                       checked={this.state.deferido === "naodeferido"}
+                                <input type="radio" name="deferido" value="false"
+                                       checked={this.state.deferido === "false"}
                                        onChange={this.handleChange}/>Não Deferir
                             </label>
                         </div>
@@ -42,11 +71,29 @@ export default class ParecerInsert extends Component {
                                     handleChange={this.handleChange} textLabel="Nº do Memorando"/>
                     </div>
                     <a href="#" target="_blank" className="pull-right">Gerar Memorando</a>
+                    <Alerta show={erro.erro} alertClass="alert alert-danger" message={erro.message}/>
                     <div className="input-group text-center">
-                        <button type="submit" className="btn btn-custom">Enviar</button>
+                        <button type="submit" className="btn btn-custom"
+                            onClick={this.handleSubmit}>Enviar</button>
                     </div>
-                </div>
+                </form>
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        requerimento: state.requerimento
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        sendParecer: parecer => dispatch(sendParecer(parecer)),
+        reloadRequerimentoAberto: parecer => reloadRequerimentoAberto(dispatch),
+        getRequerimentosEmAberto: () => dispatch(getRequerimentosEmAberto())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ParecerInsert);
