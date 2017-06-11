5a02e7bd59b85413004079125658eb58310749fc
marcelohenrique180 <marcelohs180@gmail.com>
2017-06-11 14:13:21 -0300

    hotfix(authorized-container)

--- src/client/js/containers/AuthorizedContainer.js
+++ src/client/js/containers/AuthorizedContainer.js
@@ -4,15 +4,11 @@ class AuthorizedContainer extends React.Component {
 
     componentWillMount() {
         const { authorize } = this.props.route;
-        const { router } = this.context;
+        const { router } = this.props;
 
         const role = localStorage.getItem('role');
 
-        if(role) {
-            if (authorize.indexOf(role) === -1) {
-                router.push('/nao-autorizado');
-            }
-        }else {
+        if(typeof role === "undefined" || authorize.indexOf(role) === -1) {
             router.push('/nao-autorizado');
         }
     }
