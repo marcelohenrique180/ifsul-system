b833476b2079bdcb42f85e6f6861bcab93b46455
marcelohenrique180 <marcelohs180@gmail.com>
2017-06-11 23:02:29 -0300

    feat(menu): different size, depending on path.

--- src/client/js/containers/menu/CordMenu.js
+++ src/client/js/containers/menu/CordMenu.js
@@ -6,12 +6,24 @@ import RouterHandler from '../RouterHandler'
 class CordMenu extends AuthorizedContainer {
 
     render(){
+        let menuSize = "col-xs-12 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1";
+
+        if (this.props.routes[3]){
+            const {path} = this.props.routes[3];
+
+            switch (path){
+                case "requerimento/visualizar":
+                    menuSize = "col-xs-12 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-7";
+                    break;
+            }
+        }
+
         return (
             <div>
                 <div className="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-lg-3">
                     <CordRequerimentoAberto {...this.props} />
                 </div>
-                <div className="col-xs-12 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1">
+                <div className={menuSize}>
                     <RouterHandler {...this.props} />
                 </div>
             </div>
