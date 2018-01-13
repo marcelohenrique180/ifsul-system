4febfcc3aa88d5c752daf69c1a36d9b53e090d7e
Marcelo Henrique <marcelohs180@gmail.com>
2018-01-13 15:07:17 -0200

    hotfix: tipar authorizedContainer #HOTFIX004

--- src/client/js/containers/AuthorizedContainer.js
+++ src/client/js/containers/AuthorizedContainer.js
@@ -1,7 +1,18 @@
-import React from 'react'
+// @flow
 
-class AuthorizedContainer extends React.Component {
-  componentWillMount () {
+import * as React from 'react'
+
+type Props = {
+  router: {
+    push: Function
+  },
+  route: {
+    authorize: Array<string>
+  }
+}
+
+class AuthorizedContainer extends React.Component<Props> {
+  componentWillMount() {
     const { authorize } = this.props.route
     const { router } = this.props
 
