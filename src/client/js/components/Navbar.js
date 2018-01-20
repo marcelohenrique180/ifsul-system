11cb0f88cdb85443c07a43d8b48c0853a7b1dc5f
Marcelo Henrique <marcelohs180@gmail.com>
2018-01-20 01:39:59 -0200

    style: materializar NavBar
    Escala de cores usada: Green
    (#RNF005)

--- src/client/js/components/Navbar.js
+++ src/client/js/components/Navbar.js
@@ -1,16 +1,20 @@
 // @flow
 
+import * as React from 'react'
+
 import type {
   State as DefaultState,
   Store,
   Usuario
 } from '../reducers/types/index'
-import React, { Component } from 'react'
 
+import AppBar from 'material-ui/AppBar'
 import type { Dispatch } from '../actions/types/index'
-import { Link } from 'react-router'
+import FlatButton from 'material-ui/FlatButton'
+import autobind from 'autobind-decorator'
+import { browserHistory } from 'react-router'
 import { connect } from 'react-redux'
-import { indexRoute } from '../util'
+import { green300 } from 'material-ui/styles/colors'
 import { logoutUser } from '../actions/index'
 
 type StateProps = {
@@ -24,9 +28,24 @@ type Props = StateProps &
     isAuthenticated: boolean
   }
 
-class Navbar extends Component<Props> {
+class Navbar extends React.Component<Props> {
   static defaultProps = {
     isAuthenticated: localStorage.getItem('id_token') !== null
+  }
+
+  @autobind
+  handleLogout(event: SyntheticInputEvent<HTMLInputElement>) {
+    event.preventDefault()
+
+    browserHistory.push('/')
+    this.props.logoutUser()
+  }
+
+  @autobind
+  handleLogin(event: SyntheticInputEvent<HTMLInputElement>) {
+    event.preventDefault()
+
+    browserHistory.push('/login')
   }
 
   render() {
@@ -34,28 +53,25 @@ class Navbar extends Component<Props> {
       ? this.props
       : this.props.usuario.payload
 
-    return (
-      <nav className="navbar navbar-inverse">
-        <Link to={indexRoute()} className="navbar-brand">
-          LOGO
-        </Link>
+    const rightButton = isAuthenticated ? (
+      <FlatButton
+        label="Logout"
+        backgroundColor={green300}
+        onClick={this.handleLogout}
+      />
+    ) : (
+      <FlatButton
+        label="Login"
+        backgroundColor={green300}
+        onClick={this.handleLogin}
+      />
+    )
 
-        {isAuthenticated ? (
-          <Link
-            className="nav-link"
-            to="/logout"
-            onClick={() => {
-              this.props.logoutUser()
-            }}
-          >
-            <img src="img/glyphicons-388-log-out.png" alt="" /> Logout
-          </Link>
-        ) : (
-          <Link className="nav-link" to="/login">
-            <img src="img/glyphicons-388-log-out.png" alt="" /> Login
-          </Link>
-        )}
-      </nav>
+    return (
+      <AppBar
+        style={{ backgroundColor: green300 }}
+        iconElementRight={rightButton}
+      />
     )
   }
 }
