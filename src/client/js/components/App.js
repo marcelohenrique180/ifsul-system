8428198edf41739c14a1b25f743d5e6888943bf5
Marcelo Henrique <marcelohs180@gmail.com>
2018-01-21 01:10:47 -0200

    styles: criar menu de navegação
    (#RNF005)

--- src/client/js/components/App.js
+++ src/client/js/components/App.js
@@ -3,6 +3,11 @@
 import * as React from 'react'
 
 import {
+  BottomNavigation,
+  BottomNavigationItem
+} from 'material-ui/BottomNavigation'
+import {
+  gray500,
   green400,
   green500,
   green700,
@@ -11,9 +16,15 @@ import {
   lightGreenA200
 } from 'material-ui/styles/colors'
 
+import FontIcon from 'material-ui/FontIcon'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Navbar from './Navbar'
+import autobind from 'autobind-decorator'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
+
+type States = {
+  selectedIndex: number
+}
 
 export const muiTheme = getMuiTheme({
   palette: {
@@ -26,13 +37,54 @@ export const muiTheme = getMuiTheme({
   }
 })
 
-export default class App extends React.Component<{ children: React$Node }> {
+const NavigationStyle = {
+  position: 'fixed',
+  bottom: 0
+}
+
+export default class App extends React.Component<
+  { children: React$Node },
+  States
+> {
+  state = {
+    selectedIndex: 0
+  }
+  @autobind
+  select(i: number) {
+    this.setState({ selectedIndex: i })
+  }
   render() {
+    const notificationIcon = (
+      <FontIcon className="material-icons">notifications</FontIcon>
+    )
+    const menuIcon = <FontIcon className="material-icons">menu</FontIcon>
+    const codeIcon = <FontIcon className="material-icons">code</FontIcon>
+
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <div>
+        <div style={{ marginBottom: 56 }}>
           <Navbar />
           {this.props.children}
+          <BottomNavigation
+            selectedIndex={this.state.selectedIndex}
+            style={NavigationStyle}
+          >
+            <BottomNavigationItem
+              label="Notificações"
+              icon={notificationIcon}
+              onClick={() => this.select(0)}
+            />
+            <BottomNavigationItem
+              label="Todos"
+              icon={menuIcon}
+              onClick={() => this.select(1)}
+            />
+            <BottomNavigationItem
+              color={gray500}
+              label="Em Progresso"
+              icon={codeIcon}
+            />
+          </BottomNavigation>
         </div>
       </MuiThemeProvider>
     )
