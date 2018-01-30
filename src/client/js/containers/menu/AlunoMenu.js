62e108efd5f58383d2956dccc92960e06c1bca4f
Marcelo Henrique <marcelohs180@gmail.com>
2018-01-30 10:32:08 -0200

    refactor: remover classes desnecessárias

--- src/client/js/containers/menu/AlunoMenu.js
+++ src/client/js/containers/menu/AlunoMenu.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router'
-import RouterHandler from '../RouterHandler'
 import AuthorizedContainer from '../AuthorizedContainer'
+import { Link } from 'react-router'
+import React from 'react'
+import RouterHandler from '../RouterHandler'
 
 class AlunoMenu extends AuthorizedContainer {
   render() {
     return (
       <div>
-        <div className="col-xs-10 col-sm-4 col-lg-2 col-xs-offset-1 col-sm-offset-0">
-          <div className="panel panel-default">
-            <div className="panel-heading">Requerimentos</div>
-            <div className="panel-body side-menu">
-              <ul className="list-group">
-                <li className="list-group-item side-item">
-                  <span className="glyphicon glyphicon glyphicon-plus" />
+        <div>
+          <div>
+            <div>Requerimentos</div>
+            <div>
+              <ul>
+                <li>
+                  <span />
                   <Link to="/menu/aluno/requerimento/solicitar">Solicitar</Link>
                 </li>
-                <li className="list-group-item side-item">
-                  <span className="glyphicon glyphicon glyphicon-search" />
+                <li>
+                  <span />
                   <Link to="/menu/aluno/requerimento/visualizar">
                     Visualizar
                   </Link>
@@ -26,7 +26,7 @@ class AlunoMenu extends AuthorizedContainer {
             </div>
           </div>
         </div>
-        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-sm-offset-1 col-md-offset-1 col-lg-offset-2">
+        <div>
           <RouterHandler {...this.props} />
         </div>
       </div>
