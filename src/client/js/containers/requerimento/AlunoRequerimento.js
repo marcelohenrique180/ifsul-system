efdfcc9e37ad5d3bfa6beec31672d3fb42cba2d5
Marcelo Henrique <marcelohs180@gmail.com>
2018-02-02 06:18:10 -0200

    refactor: separar responsabilidades | Requerimento

--- src/client/js/containers/requerimento/AlunoRequerimento.js
+++ src/client/js/containers/requerimento/AlunoRequerimento.js
@@ -15,6 +15,7 @@ import type {
 import Alerta from '../../components/Alerta'
 import AlunoInfo from '../../containers/aluno/AlunoInfo'
 import Carregando from '../../components/Carregando'
+import RequerimentoInsert from './RequerimentoInsert'
 import type { SendRequerimento } from '../../actions/requerimento'
 import { areFieldsEmpty } from '../../util'
 import autobind from 'autobind-decorator'
@@ -23,14 +24,6 @@ import { requestAluno } from '../../actions/aluno'
 import { requestCursos } from '../../actions/curso'
 import { requestTipos } from '../../actions/tipo'
 import { sendRequerimento } from '../../actions/requerimento'
-
-const defaultState = {
-  tipo: '',
-  requerimento: '',
-  justificativa: '',
-  erro: { erro: false, message: '' },
-  enviado: false
-}
 
 type StateProps = {
   aluno: DefaultState<Aluno>,
@@ -51,73 +44,19 @@ type Props = StateProps &
     router: { push: string => void }
   }
 
-type State = {
-  tipo: string,
-  requerimento: string,
-  justificativa: string,
-  erro: { erro: boolean, message: string },
-  enviado: boolean
-}
-
-class AlunoRequerimento extends React.Component<Props, State> {
+class AlunoRequerimento extends React.Component<Props> {
   constructor(props: Props) {
     super(props)
-    this.state = defaultState
 
     this.props.requestTipos()
     this.props
       .requestAluno()
       .then(
         aluno =>
-          aluno.payload !== null && typeof aluno.payload !== 'undefined'
+          aluno.payload
             ? this.props.requestCursos(aluno.payload._links.curso.href)
             : null
       )
-  }
-
-  renderTipos() {
-    if (
-      typeof this.props.tipos.payload._embedded !== 'undefined' &&
-      typeof this.props.tipos.payload !== 'undefined'
-    ) {
-      return this.props.tipos.payload._embedded.tipos.map(tipo => {
-        return (
-          <option key={tipo._links.self.href} value={tipo._links.self.href}>
-            {tipo.tipo}
-          </option>
-        )
-      })
-    }
-    return ''
-  }
-
-  @autobind
-  handleSubmit(e: SyntheticInputEvent<HTMLInputElement>) {
-    const { tipo, requerimento, justificativa } = this.state
-    const notNullFields = [tipo, requerimento, justificativa]
-    const aluno = this.props.aluno.payload._links.aluno.href
-
-    const error = areFieldsEmpty(notNullFields)
-    if (error) {
-      this.setState({
-        erro: {
-          erro: true,
-          message: 'Algum campo obrigatório não foi preenchido.'
-        }
-      })
-    } else {
-      this.setState({ erro: { erro: false, message: '' } })
-      this.props
-        .sendRequerimento({ tipo, requerimento, justificativa, aluno })
-        .then(() => this.setState({ enviado: true }))
-    }
-    e.preventDefault()
-  }
-
-  @autobind
-  onVoltar() {
-    this.setState(defaultState)
-    this.props.router.push('/menu/aluno/requerimento/visualizar')
   }
 
   @autobind
@@ -128,10 +67,12 @@ class AlunoRequerimento extends React.Component<Props, State> {
   }
 
   render() {
-    const tiposState = this.props.tipos
-    const requerimentoProp = this.props.requerimento
+    const { curso, aluno, tipos } = this.props
+    const fetched = curso.fetched && aluno.fetched && tipos.fetched
 
-    const { tipo, requerimento, justificativa, erro, enviado } = this.state
+    if (!fetched || !tipos.payload._embedded) {
+      return <Carregando />
+    }
 
     return (
       <div>
@@ -140,87 +81,17 @@ class AlunoRequerimento extends React.Component<Props, State> {
             <h3 className="panel-title">Criar Requerimento</h3>
           </div>
           <div className="panel-body">
-            {enviado ? (
-              <div>
-                <div className="row">
-                  <Alerta
-                    show={true}
-                    alertClass="alert-success"
-                    message="Enviado com sucesso."
+            <div className="container-fluid">
+              <div className="row">
+                <div className="form-group col-centered">
+                  <AlunoInfo />
+                  <RequerimentoInsert
+                    tipos={tipos.payload._embedded.tipos}
+                    aluno={aluno.payload}
                   />
                 </div>
-                <div className="text-center">
-                  <button className="btn btn-custom" onClick={this.onVoltar}>
-                    Voltar
-                  </button>
-                </div>
               </div>
-            ) : (
-              <div className="container-fluid">
-                <div className="row">
-                  {tiposState.fetched && (
-                    <div className="form-group col-centered">
-                      <AlunoInfo />
-                      <div className="form-group">
-                        <div>
-                          <label htmlFor="tipo">Tipo</label>
-                          <select
-                            name="tipo"
-                            id="tipo"
-                            className="form-control"
-                            value={tipo}
-                            onChange={this.handleChange}
-                          >
-                            <option value="" />
-                            {this.renderTipos()}
-                          </select>
-                        </div>
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="requerimento">Requerimento</label>
-                        <textarea
-                          name="requerimento"
-                          id="requerimento"
-                          rows="5"
-                          className="form-control"
-                          value={requerimento}
-                          onChange={this.handleChange}
-                        />
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="justificativa">Justificativa</label>
-                        <textarea
-                          name="justificativa"
-                          id="justificativa"
-                          rows="5"
-                          className="form-control"
-                          value={justificativa}
-                          onChange={this.handleChange}
-                        />
-                      </div>
-                      <Alerta
-                        show={erro.erro}
-                        message={erro.message}
-                        alertClass="alert-danger"
-                      />
-                      <div className="input-group text-center">
-                        {requerimentoProp.isFetching ? (
-                          <Carregando />
-                        ) : (
-                          <button
-                            type="submit"
-                            className="btn btn-custom"
-                            onClick={this.handleSubmit}
-                          >
-                            Enviar
-                          </button>
-                        )}
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </div>
-            )}
+            </div>
           </div>
         </div>
       </div>
---
+++ src/client/js/containers/requerimento/RequerimentoInsert.js
@@ -0,0 +1,154 @@
+// @flow
+
+/** Exibir 3 campos
+    - select tipos
+    - textarea requerimentos
+    - textarea justificativa
+    - raisedButton enviar
+
+    Receber props tipos, aluno
+
+    importar sendRequerimento para enviar onSubmit
+*/
+
+import * as React from 'react'
+
+import type { Action, Dispatch } from '../../actions/types/index'
+import type { Aluno, Requerimento, Tipo } from '../../reducers/types/index'
+
+import MenuItem from 'material-ui/MenuItem'
+import RaisedButton from 'material-ui/RaisedButton'
+import SelectField from 'material-ui/SelectField'
+import type { SendRequerimento } from '../../actions/requerimento'
+import Subheader from 'material-ui/Subheader'
+import TextField from 'material-ui/TextField'
+import { areFieldsEmpty } from '../../util'
+import autobind from 'autobind-decorator'
+import { connect } from 'react-redux'
+import { sendRequerimento } from '../../actions/requerimento'
+
+type DispatchProps = {
+  sendRequerimento?: SendRequerimento => Promise<Action<Requerimento>>
+}
+
+type Props = {
+  tipos: Array<Tipo>,
+  aluno: Aluno
+} & DispatchProps
+
+type State = {
+  tipo: string,
+  requerimento: string,
+  justificativa: string,
+  erro: { erro: boolean, message: string },
+  enviado: boolean,
+  formComplete: boolean
+}
+
+@connect(null, mapDispatchToProps)
+class AlunoRequerimento extends React.Component<Props, State> {
+  state = {
+    tipo: '',
+    requerimento: '',
+    justificativa: '',
+    erro: { erro: false, message: '' },
+    enviado: false,
+    formComplete: false
+  }
+
+  @autobind
+  handleSubmit(event: SyntheticInputEvent<HTMLInputElement>) {
+    event.preventDefault()
+
+    const { tipo, requerimento, justificativa } = this.state
+    const aluno = this.props.aluno._links.aluno.href
+
+    if (this.props.sendRequerimento) {
+      this.props
+        .sendRequerimento({ tipo, requerimento, justificativa, aluno })
+        .then(() => this.setState({ enviado: true }))
+    }
+  }
+
+  @autobind
+  renderTipos() {
+    return this.props.tipos.map(tipo => {
+      return (
+        <MenuItem
+          key={tipo._links.self.href}
+          value={tipo._links.self.href}
+          primaryText={tipo.tipo}
+        />
+      )
+    })
+  }
+
+  @autobind
+  handleChange(event: SyntheticInputEvent<HTMLInputElement>) {
+    const { value, name } = event.target
+
+    const { tipo, requerimento, justificativa } = {
+      ...this.state,
+      [name]: value
+    }
+    const error = areFieldsEmpty([tipo, requerimento, justificativa])
+    this.setState({ formComplete: !error, [name]: value })
+  }
+
+  handleSelectChange = (e: any, i: number, tipo: string) =>
+    this.setState({ tipo })
+
+  render() {
+    return (
+      <div>
+        <Subheader>Requerimento</Subheader>
+        <SelectField
+          floatingLabelText="Tipos"
+          value={this.state.tipo}
+          onChange={this.handleSelectChange}
+        >
+          {this.renderTipos()}
+        </SelectField>
+        <TextField
+          name="requerimento"
+          rows={1}
+          floatingLabelText="Requerimento"
+          multiLine={true}
+          floatingLabelStyle={{ color: 'rgba(0, 0, 0, 0.57)' }}
+          defaultValue={this.state.requerimento}
+          onChange={this.handleChange}
+          fullWidth={true}
+        />
+        <TextField
+          name="justificativa"
+          rows={1}
+          floatingLabelText="Justificativa"
+          multiLine={true}
+          floatingLabelStyle={{ color: 'rgba(0, 0, 0, 0.57)' }}
+          defaultValue={this.state.justificativa}
+          onChange={this.handleChange}
+          fullWidth={true}
+        />
+        <RaisedButton
+          primary={true}
+          label="Enviar"
+          onClick={this.handleSubmit}
+          disabled={!this.state.formComplete}
+          style={{
+            display: 'block',
+            width: '4em',
+            margin: '0 auto'
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch: Dispatch): DispatchProps {
+  return {
+    sendRequerimento: requerimento => dispatch(sendRequerimento(requerimento))
+  }
+}
+
+export default AlunoRequerimento
